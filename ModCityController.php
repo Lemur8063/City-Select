e3ad0cd1d62db3aabd4758ad6298e07eef897da0
Valahanovich Oleg <oleg_valahanovich@mail.ru>
2021-08-03 00:14:33 +0300

    update

--- ModCityController.php
+++ ModCityController.php
@@ -7,12 +7,6 @@ class  ModCityController extends Common {
     function action_index () {
             require_once("index.php");
     }
-
-    public function getDb() {
-        /*$this->db->query('SELECT * FROM `city_select`')->fetchAll();
-        var_dump($this);*/
-    }
-
 }
 
 
--- index.php
+++ index.php
@@ -1,108 +1,177 @@
 <?php
-require_once ('ModCityController.php');
+
+$table = $this->db->query('SELECT * FROM `city_select`')->fetchAll();
+
+if ($_GET['remId']) {
+    $id = $_GET['remId'];
+
+    $this->db->query('DELETE FROM `city_select` where c_id="' . $id . '"');
+
+    echo 'work';
+
+}
+if ($_GET['addId']) {
+
+    $this->db->query('INSERT INTO `city_select` (`c_id`, `c_name`, `c_content`) VALUES (NULL, NULL, NULL)');
+
+    echo 'work';
+}
+
+if ($_GET['c_id'] && $_GET['c_name'] && $_GET['c_content']) {
+
+    $this->db->query("UPDATE `city_select` SET `c_name` = '" . $_GET['c_name'] . "', `c_content` = '" . $_GET['c_content'] . "' WHERE `city_select`.`c_id` = " . $_GET['c_id'] . " ");
+
+    echo 'work';
+}
 
 ?>
-
-
 <h1>Добро пожаловать !</h1>
-<div>
-    <div class="row"></div>
 
-    <table id="listmod" class="sTable" width="100%">
-        <thead class="headerHead">
-        <tr class="servicePanel">
-            <th colspan="100">
-                <div class="recordNum">
-                    <b>Всего:</b><!--&nbsp;1-->
-                </div>
+<table id="listmod" class="sTable" width="100%">
+    <thead class="headerHead">
+    <tr class="servicePanel">
+        <th colspan="100">
+            <div class="recordNum">
+                <b>Всего:</b>&nbsp;<?= count($table) ?>
+            </div>
+            <div style="float:right;"></div>
+        </th>
+    </tr>
+    <tr class="headerText">
+        <td rowspan="1" width="1%">№</td>
+        <td style="cursor:pointer;" width="250">
+            <div>Город
                 <div style="float:right;"></div>
-            </th>
-        </tr>
-        <tr class="headerText">
-            <td rowspan="1" width="1%">№</td>
-            <td style="cursor:pointer;" width="250">
-                <div>Город<div style="float:right;"></div></div>
-            </td>
+            </div>
+        </td>
 
-            <td style="cursor:pointer;" width="150">
-                <div>Описание<div style="float:right;"></div></div>
-            </td>
-        </tr>
-        </thead>
+        <td style="cursor:pointer;" width="150">
+            <div>Описание
+                <div style="float:right;"></div>
+            </div>
+        </td>
+    </tr>
+    </thead>
+    <?php foreach ($table as $item) { ?>
 
-
-        <tbody class="city_row">
-        <tr class="pointer"><td>1</td>
-            <td width="50">Минск</td>
-            <td width="350">Минск (белор. Мінск) — столица Белоруссии, административный центр Минской области и Минского района, в состав которых не входит, поскольку является самостоятельной административно-территориальной единицей с особым (столичным) статусом.</td>
-        </tbody>
-
-        <tbody class="city_row">
-        <tr class="pointer"><td>1</td>
-            <td width="50">Брест</td>
-            <td width="350">Брест (белор. Брэст) — город на юго-западе Белоруссии, административный центр Брестской области и Брестского района.</td>
-        </tbody>
-
-        <tfoot>
-        <tr>
-            <td colspan="100" style="padding:0">
-                <table class="paginTable" id="pagin_mod" width="100%">
-                    <tbody><tr>
-
-
-                        <td width="100%" align="center">
-
-                            <!-- 1 из 1 -->
-
-                        </td>
+        <?php echo '<tbody class="city_row">
+                    <tr id=' . $item['c_id'] . ' class="c_id pointer"><td>' . $item['c_id'] . '</td>
+                    <td class="edit c_name" width="50">' . $item['c_name'] . '</td>
+                    <td class="edit c_content" width="350">' . $item['c_content'] . '</td>
                     </tr>
                     </tbody>
+                    ' ?>
+    <?php } ?>
 
-                </table>
-            </td>
-        </tr>
-        </tfoot>
-    </table>
-    <div style="margin-top: 20px;">
-        <div style="float:left;">
-            <input type="button" class="button" value="Добавить" onclick="">
-        </div>
-        <div style="float:left;">
-            <input type="button" class="button" value="Подтвердить" onclick="">
-        </div>
-        <div style="float:left;">
-            <input type="button" class="button" value="Удалить" onclick="load()">
-        </div>
+    <!--<tfoot>
+    <tr>
+        <td colspan="100" style="padding:0">
+            <table class="paginTable" id="pagin_mod" width="100%">
+
+                <tbody><tr>
+                    <td width="100%" align="center">
+                        1 из 1
+                    </td>
+                </tr>
+                </tbody>
+
+            </table>
+        </td>
+    </tr>
+    </tfoot>-->
+
+</table>
+<div style="margin-top: 20px;">
+    <div style="float:left;">
+        <input type="button" class="button" id="add_btn" value="Добавить">
     </div>
+    <div style="float:left;">
+        <input type="button" class="button edit_btn req_false" id="edit_btn" value="Редактировать">
+    </div>
+    <div style="float:left;">
+        <input type="button" class="button" id="del_btn" value="Удалить">
+    </div>
+</div>
 
 
-    <style>
-        .active {
-            background-color: #6a8caf !important;
-        }
-        .city_row:hover {
-            background-color: #6a8caf !important;
-        }
-        .table.sTable td {
-            pointer-events: none;
-        }
-    </style>
+<style>
+    .active_item {
+        background-color: #a5a9b6 !important;
+    }
+
+    .city_row:hover {
+        background-color: #a5a9b6 !important;
+    }
+
+    .table.sTable td {
+        pointer-events: none;
+    }
+</style>
 
 
+<script>
 
-    <script>
-        $(document).ready(function () {
-            $('.city_row').click(function () {
-                $('.city_row').removeClass('active');
-                $(this).addClass('active');
-            })
+    $(document).ready(function () {
+        $('.city_row').click(function () {
+            $('.city_row').removeClass('active_item');
+            $(this).addClass('active_item');
+        })
 
-
-
-
-            $('.item_add').click(function () {
-                $('.city').append("<td contenteditable='true'></td>");
-                $('.description').append("<td contenteditable='true'></td>");
+        $('#del_btn').click(function () {
+            let remId = 'remId=' + $('.city_row.active_item > tr').attr('id');
+            $.ajax({
+                type: "GET",
+                url: "index.php?module=city",
+                data: remId,
+                success: function () {
+                    location.reload();
+                }
             })
         })
-    </script>
+        $('#add_btn').click(function () {
+            let addItem = 'addId=' + 1;
+            $.ajax({
+                type: "GET",
+                url: "index.php?module=city",
+                data: addItem,
+                success: function () {
+                    location.reload();
+                }
+            })
+        })
+
+        $('#edit_btn').click(function () {
+            if ($('#edit_btn').hasClass('req_false')) {
+                if (!$('#edit_item').length) {
+                    $('.city_row.active_item .edit').attr('contenteditable', 'true').addClass('edit_item')
+                    $('.city_row.active_item').addClass('edit_item')
+                    $(this).removeClass('req_false')
+                    if ($('.city_row.active_item').hasClass('edit_item')) {
+                        $('#edit_btn').val('Ok');
+                    }
+                } else {
+                }
+            } else {
+
+                let c_id = $('.edit_item .c_id').attr('id');
+                console.log(c_id);
+                let c_name = $('.edit_item .c_name').text();
+                console.log('c_name');
+                let c_content = $('.edit_item .c_content').text();
+                $.ajax({
+                    type: "GET",
+                    url: "index.php?module=city",
+                    data: {
+                        c_id: c_id,
+                        c_name: c_name,
+                        c_content: c_content,
+                    },
+                    success: function () {
+                        location.reload();
+                    }
+                })
+            }
+        })
+
+    })
+</script>
