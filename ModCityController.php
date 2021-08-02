62f01e0a6509986b45dfc9a301f225b7776ef302
Valahanovich Oleg <oleg_valahanovich@mail.ru>
2021-08-03 00:25:36 +0300

    fix

--- ModCityController.php
+++ ModCityController.php
@@ -5,7 +5,7 @@ require_once ('core2/inc/CoreController.php');
 class  ModCityController extends Common {
 
     function action_index () {
-            require_once("index.php");
+            require_once("core2/mod/city/v1.0.0/index.php");
     }
 }
 
